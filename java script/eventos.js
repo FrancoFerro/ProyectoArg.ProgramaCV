4598c0dffce09e533886911480199b7e8b9eae2b
Franco Ezequiel Ferro <89946667+FrancoFerro@users.noreply.github.com>
2023-02-28 22:37:30 -0300

    Add files via upload

---
+++ java script/eventos.js
@@ -0,0 +1,62 @@
+function enCeleste1() {
+    Element =document.getElementById("pers");
+    Element.src="img/persona2.png";
+    }
+    function enVioleta1() {
+        Element =document.getElementById("pers");
+        Element.src="img/persona.png";
+    }
+    
+    function enCeleste2() {
+        Element =document.getElementById("labo");
+        Element.src="img/laboral2.png";
+    }
+     function enVioleta2() {
+        Element =document.getElementById("labo");
+        Element.src="img/laboral.png";
+    }
+    
+    function enCeleste3() {
+        Element =document.getElementById("habi");
+        Element.src="img/habilidades2.png";
+    }
+    function enVioleta3() {
+        Element =document.getElementById("habi");
+        Element.src="img/habilidades.png";
+    }
+    
+    function enCeleste4() {
+        Element =document.getElementById("estu");
+        Element.src="img/estudios2.png";
+    }
+    function enVioleta4() {
+        Element =document.getElementById("estu");
+        Element.src="img/estudios.png";
+    }
+    
+    function enAmarillo5() {
+        Element =document.getElementById("be");
+        Element.src="img/behance2.png";
+    }
+    function enNegro5() {
+        Element =document.getElementById("be");
+        Element.src="img/behance.png";
+    }
+    
+    function enAmarillo6() {
+        Element =document.getElementById("gi");
+        Element.src="img/github2.png";
+    }
+    function enNegro6() {
+        Element =document.getElementById("gi");
+        Element.src="img/github.png";
+    }
+    
+    function enAmarillo7() {
+        Element =document.getElementById("li");
+        Element.src="img/linkedin2.png";
+    }
+    function enNegro7() {
+        Element =document.getElementById("li");
+        Element.src="img/linkedin.png";
+    }
